7f07af5079133d15d39d9becd500b3a3c8b69170
Silvia <sarnaizgarcia@gmail.com>
2019-01-15 15:29:46 +0100

    ejercicio  1-bucles/1-bucle-for.js

--- 1-bucles/1-bucle-for.js
+++ 1-bucles/1-bucle-for.js
@@ -1,3 +1,12 @@
 // ej 1
 // haz un bucle que muestre por consola los números del 4 al 9 inclusive
 // Utilizad el bucle for
+
+/* for (var i = 0; i < 9; i++) {
+    n += i;
+    mifuncion(n);
+} */
+for (var i = 4; i < 9; i++) {
+    console.log(i + 1);
+}
+
