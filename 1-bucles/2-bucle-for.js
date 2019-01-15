8a52883ab4220b71003ae0a6f17e056ced725f01
Silvia <sarnaizgarcia@gmail.com>
2019-01-15 15:54:22 +0100

    1-bucles/2-bucle-for.js

--- 1-bucles/2-bucle-for.js
+++ 1-bucles/2-bucle-for.js
@@ -1,3 +1,8 @@
 // ej 2
 // haz un bucle que muestre por consola los impares del 3 al 17 inclusive
 // Utilizad el bucle for
+ for (var i = 3; i < 18; i++) {
+     if (i % 2 == 1) {
+         console.log(i);
+     };
+ }
