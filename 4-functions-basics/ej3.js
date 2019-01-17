7626eccf0161d44af4b23066161da2036da29e70
Silvia <sarnaizgarcia@gmail.com>
2019-01-17 21:13:45 +0100

    ejercicio 4-functions-basics/ej3.js resuelto

--- 4-functions-basics/ej3.js
+++ 4-functions-basics/ej3.js
@@ -10,9 +10,18 @@
 
 let caracterInicial = (texto) => {
     // Aquí tu código.  Desde aquí:
-    
+
+    if (texto === '') {
+        return 'Debo ser ejecutada con un string no vacío';
+    } else if
+    (typeof texto === 'string')
+    {
+        return texto.charAt(0);
+    } else if
+    (typeof texto !== 'string') {
+        return 'Debo ser ejecutada con un string';
+    }}
     // Hasta aquí.
-}
 
 
 let test = require('../test.js');
